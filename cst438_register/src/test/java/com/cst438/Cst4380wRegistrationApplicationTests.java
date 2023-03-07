ffa26717c0e5588f317a29ec9b318afb134276a8
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2023-03-07 07:47:59 -0800

    Finished test cases, still buggy. Pending TA visit for corrections or clarification

--- cst438_register/src/test/java/com/cst438/Cst4380wRegistrationApplicationTests.java
+++ cst438_register/src/test/java/com/cst438/Cst4380wRegistrationApplicationTests.java
@@ -8,10 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -25,20 +29,31 @@ import com.cst438.domain.StudentRepository;
 import com.cst438.service.GradebookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest
+@ContextConfiguration(classes = { ScheduleController.class })
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest
 class Cst4380wRegistrationApplicationTests {
 	
 	static final String URL = "http://localhost:8080";
 	public static final int TEST_STUDENT_ID = 10;
-	public static final String TEST_STUDENT_EMAIL = "test@csumb.edu";
-	public static final String TEST_STUDENT_NAME  = "test";
-	public static final int TEST_STATUS_CODE = 1;
-	public static final String TEST_STATUS = "HOLD";
+	public static final String TEST_STUDENT_EMAIL = "james@csumb.edu";
+	public static final String TEST_STUDENT_NAME  = "james";
+	public static final int TEST_STATUS_CODE = 0;
+	public static final String TEST_STATUS = "";
 
 
 	@MockBean
+    CourseRepository courseRepository;
+	
+	@MockBean
 	StudentRepository studentRepository;
+	
+	@MockBean
+	EnrollmentRepository enrollmentRepository;
 
+	@MockBean
+	GradebookService gradebookService;
+	
 	@Autowired
 	private MockMvc mvc;
 	
@@ -57,7 +72,7 @@ class Cst4380wRegistrationApplicationTests {
 		given(studentRepository.findByEmail(TEST_STUDENT_EMAIL)).willReturn(testStudent);
 		
 		StudentDTO testDTO = ScheduleController.createStudentDTO(testStudent);
-		
+		System.out.println(asJsonString(testDTO));
 		response = mvc.perform(
 				MockMvcRequestBuilders
 			      .post("/addStudent")
@@ -68,6 +83,54 @@ class Cst4380wRegistrationApplicationTests {
 		
 		// verify that return status = OK (value 200) 
 		assertEquals(200, response.getStatus());
+		
+		StudentDTO result = fromJsonString(response.getContentAsString(), StudentDTO.class);
+        assertNotEquals( 0  , result.student_id);
+        
+        verify(studentRepository).save(any(Student.class));
+	}
+	
+	@Test
+	void placeRemoveHold() throws Exception {
+		MockHttpServletResponse response;
+		
+		Student testStudent = new Student();
+		testStudent.setStudent_id(TEST_STUDENT_ID);
+		testStudent.setEmail(TEST_STUDENT_EMAIL);
+		testStudent.setName(TEST_STUDENT_NAME);
+		testStudent.setStatusCode(TEST_STATUS_CODE);
+		testStudent.setStatus(TEST_STATUS);
+		
+		given(studentRepository.findByEmail(TEST_STUDENT_EMAIL)).willReturn(testStudent);
+		
+		String student_email = testStudent.getEmail();
+		response = mvc.perform(
+				MockMvcRequestBuilders
+			      .get("/placeStudentHold/"+student_email)
+			      .contentType(MediaType.APPLICATION_JSON)
+			      .accept(MediaType.APPLICATION_JSON))
+				.andReturn().getResponse();
+		
+		// verify that return status = OK (value 200) 
+		assertEquals(200, response.getStatus());
+		
+		StudentDTO result = fromJsonString(response.getContentAsString(), StudentDTO.class);
+        assertNotEquals( 0  , result.student_id);
+        
+        verify(studentRepository).save(any(Student.class));
+        
+        response = mvc.perform(
+				MockMvcRequestBuilders
+			      .get("/removeStudentHold/"+student_email)
+			      .contentType(MediaType.APPLICATION_JSON)
+			      .accept(MediaType.APPLICATION_JSON))
+				.andReturn().getResponse();
+		
+		// verify that return status = OK (value 200) 
+		assertEquals(200, response.getStatus());
+		
+		result = fromJsonString(response.getContentAsString(), StudentDTO.class);
+        assertNotEquals( 0  , result.student_id);
 	}
 
 
@@ -75,6 +138,14 @@ class Cst4380wRegistrationApplicationTests {
 		try {
 
 			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private static <T> T  fromJsonString(String str, Class<T> valueType ) {
+		try {
+			return new ObjectMapper().readValue(str, valueType);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
